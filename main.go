982ba49d639befa0aa2cad45f2f3b0c21a56c349
Pina641 <kakinotane2048@gmail.com>
2023-11-21 19:21:03 +0900

    main.go を配信できるように変更

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"go-backend-template/internal/handler"
-	"go-backend-template/internal/migration"
-	"go-backend-template/internal/pkg/config"
-	"go-backend-template/internal/repository"
+	"fmt"
+
+	"github.com/Kaki256/multiple-word-search-backend/internal/handler"
+	"github.com/Kaki256/multiple-word-search-backend/internal/migration"
+	"github.com/Kaki256/multiple-word-search-backend/internal/pkg/config"
+	"github.com/Kaki256/multiple-word-search-backend/internal/repository"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	"net/http"
 )
+
+type Word struct {
+	Name string `json:"name"`
+}
+
+type Dictionary struct {
+	Words []Word `json:"words"`
+}
 
 func main() {
 	e := echo.New()
@@ -17,6 +29,9 @@ func main() {
 	// middlewares
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
+	e.Use(middleware.CORS())
+
+	e.POST("/api/dictionary", postDictionaryHandler)
 
 	// connect to database
 	db, err := sqlx.Connect("mysql", config.MySQL().FormatDSN())
@@ -39,4 +54,14 @@ func main() {
 	h.SetupRoutes(v1API)
 
 	e.Logger.Fatal(e.Start(config.AppAddr()))
+}
+
+func postDictionaryHandler(c echo.Context) error {
+	dict := &Dictionary{}
+	err := c.Bind(dict)
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("%+v", err))
+	}
+	return c.JSON(http.StatusOK, dict)
 }
