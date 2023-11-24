edec80a847b0bdd5e0b93454896724872b42c0b2
Pina641 <kakinotane2048@gmail.com>
2023-11-24 21:40:23 +0900

    検索結果出ないけど一応完成にしておこうかな、今日は漬けマグロおにぎりを食べました

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
+	"log"
 	"os"
+	"regexp"
 
 	"github.com/Kaki256/multiple-word-search-backend/internal/handler"
 	"github.com/Kaki256/multiple-word-search-backend/internal/migration"
@@ -21,13 +25,27 @@ type Word struct {
 	Name       string `json:"name" db:"name"`
 	Length     int    `json:"length" db:"length"`
 	Sort       string `json:"sort" db:"sort"`
-	IsUnvoiced bool   `json:"is_unvoiced" db:"is_unvoiced"`
+	IsUnvoiced int    `json:"is_unvoiced" db:"is_unvoiced"`
 	Dictionary string `json:"dictionary" db:"dictionary"`
 }
 
-const dictionaryName = "Basic"
+var db *sqlx.DB
 
 func main() {
+	var conf = mysql.Config{
+		User:      os.Getenv("DB_USERNAME"),
+		Passwd:    os.Getenv("DB_PASSWORD"),
+		Addr:      os.Getenv("DB_HOSTNAME") + ":" + os.Getenv("DB_PORT"),
+		DBName:    os.Getenv("DB_DATABASE"),
+		Collation: "utf8mb4_general_ci",
+	}
+	var err error
+	db, err = sqlx.Open("mysql", conf.FormatDSN())
+	if err != nil {
+		echo.New().Logger.Fatal(err)
+	}
+	defer db.Close()
+
 	e := echo.New()
 
 	// middlewares
@@ -41,7 +59,7 @@ func main() {
 		return c.String(http.StatusOK, "pong\n")
 	})
 
-	e.GET("/search:strRegEx", wordSearchRegExHandler)
+	e.POST("/search/:dict", wordSearchRegExHandler)
 
 	// connect to database
 	db, err := sqlx.Connect("mysql", config.MySQL().FormatDSN())
@@ -67,17 +85,36 @@ func main() {
 }
 
 func wordSearchRegExHandler(c echo.Context) error {
-	searchKey := c.Param("strRegEx")
-	dictoinaryName := c.QueryParam("dictName")
+	dictionaryName := c.Param("dict")
+	var searchKey string
+	err := c.Bind(searchKey)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("%+v", err))
+	}
 
-}
+	var dict []Word
+	err = db.Get(&dict, "SELECT * FROM words WHERE dictionary = ", dictionaryName)
+	if errors.Is(err, sql.ErrNoRows) {
+		log.Printf("no such dictionary name = '%s'\n", dictionaryName)
+	}
 
-func getDictionaryNameHandler(c echo.Context) error {
-	dictionaryName := c.QueryParam("dictName")
+	var result []Word
+	regEx, err := regexp.Compile(searchKey)
+	if err != nil {
+		log.Printf(":(\n")
+	}
+
+	for _, word := range dict {
+		if regEx.MatchString(word.Name) {
+			result = append(result, word)
+		}
+	}
+
+	return c.JSON(http.StatusOK, result)
 }
 
 func postMakeDictionaryHandler(c echo.Context) error {
-	echo.New().GET("/dictionary/name", getDictionaryNameHandler)
+	dictionaryName := c.QueryParam("dict")
 	var dict []Word
 	err := c.Bind(dict)
 
@@ -85,21 +122,22 @@ func postMakeDictionaryHandler(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("%+v", err))
 	}
 
-	conf := mysql.Config{
-		User:      os.Getenv("DB_USERNAME"),
-		Passwd:    os.Getenv("DB_PASSWORD"),
-		Addr:      os.Getenv("DB_HOSTNAME") + ":" + os.Getenv("DB_PORT"),
-		DBName:    os.Getenv("DB_DATABASE"),
-		Collation: "utf8mb4_general_ci",
-	}
-	db, err := sqlx.Open("mysql", conf.FormatDSN())
-	if err != nil {
-		echo.New().Logger.Fatal(err)
-	}
-	defer db.Close()
-
 	// 辞書を作る
 	_, err = db.Exec("INSERT INTO dictionary name VALUES ?", dictionaryName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	insert := "INSERT INTO words(name, length, sort, is_unvoiced, dictionary) VALUES "
+
+	for _, word := range dict {
+		insert += fmt.Sprintf("('%s', %d, '%s', %d, '%s')", word.Name, word.Length, word.Sort, word.IsUnvoiced, dictionaryName)
+	}
+
+	_, err = db.Exec(insert)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	return c.JSON(http.StatusOK, dict)
 }
